759b3c04c20fbfa6c78d49628a3ad28951c709b7
Oleg Prokofev <prokofev.o.a@gmail.com>
2022-06-13 20:14:21 +0300

    Extracted merge sort into separate file

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "merge_sort.h"
 
 void GenerateArray(double A[], size_t n);
 void ShuffleArray(double A[], size_t n);
@@ -16,14 +17,11 @@ int main(int argc, const char * argv[]) {
     int const numberscount = 1000;
     static double arraytosort[numberscount] = {};
     GenerateArray(arraytosort, numberscount);
-    //PrintArray(arraytosort, numberscount);
     CheckIsSorted(arraytosort, numberscount);
     ShuffleArray(arraytosort, numberscount);
-    //PrintArray(arraytosort, numberscount);
     CheckIsSorted(arraytosort, numberscount);
     MergeSort(arraytosort, 0, numberscount-1);
     CheckIsSorted(arraytosort, numberscount);
-    //PrintArray(arraytosort, numberscount);
     return 0;
 }
 
@@ -75,75 +73,4 @@ void CheckIsSorted(double A[], size_t n)
         }
     }
     printf("\nArray is sorted\n");
-}
-
-// Merge sort function, is called with recursion
-void MergeSort(double A[], size_t l, size_t r)
-{
-    if(l < r)
-    {
-        size_t m = l + (r - l) / 2;
-        MergeSort(A, l, m);
-        MergeSort(A, m + 1, r);
-        Merge(A, l, m, r);
-        
-    }
-    
-}
-
-// Merge function which compares splited numbers and writes them to final array
-void Merge(double A[], size_t l, size_t m, size_t r)
-{
-    // Calculating number of elements of left array
-    size_t n1 = m - l + 1;
-    // Calculating number of elements of right array
-    size_t n2 = r - m;
-    
-    size_t i;
-    size_t j;
-    // First element to fill in final array is the left point
-    size_t k = l;
-    
-    // Creating subarrays
-    double L[n1];
-    double R[n2];
-    
-    // Filling left subarray with elements
-    for (i = 0; i < n1; i++) {
-        L[i] = A[l + i];
-    }
-    
-    // Filling right subarray with elements
-    for (j = 0; j < n2; j++) {
-        R[j] = A[m + 1 + j];
-    }
-    
-    i = 0;
-    j = 0;
-    // Comparing numbers and filling final array
-    while (i < n1 && j < n2) {
-        if(L[i] <= R[j])
-        {
-            A[k] = L[i];
-            i++;
-        } else
-        {
-            A[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-    
-    // Filling the rest elements if there are any
-    while (i < n1) {
-        A[k] = L[i];
-        i++;
-        k++;
-    }
-    
-    while (j < n2) {
-        A[k] = R[j];
-        j++;
-        k++;
-    }
 }
---
+++ merge_sort.h
@@ -0,0 +1,9 @@
+
+#ifndef merge_sort_h
+#define merge_sort_h
+
+#include <stdlib.h>
+
+void MergeSort(double A[], size_t l, size_t r);
+
+#endif /* merge_sort_h */
