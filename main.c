b490ad3aeaaf81ccf35adc17b0626ba2ddaccf4f
Oleg Prokofev <prokofev.o.a@gmail.com>
2022-06-13 14:10:39 +0300

    Implemented merge sort

--- main.c
+++ main.c
@@ -5,16 +5,21 @@ void GenerateArray(double A[], size_t n);
 void ShuffleArray(double A[], size_t n);
 void PrintArray(double A[], size_t n);
 void CheckIsSorted(double A[], size_t n);
+void MergeSort(double A[], size_t l, size_t r);
+void Merge(double A[], size_t l, size_t m, size_t r);
 
 int main(int argc, const char * argv[]) {
     int const numberscount = 1000;
-    double arraytosort[numberscount] = {};
+    static double arraytosort[numberscount] = {};
     GenerateArray(arraytosort, numberscount);
-    PrintArray(arraytosort, numberscount);
+    //PrintArray(arraytosort, numberscount);
     CheckIsSorted(arraytosort, numberscount);
     ShuffleArray(arraytosort, numberscount);
+    //PrintArray(arraytosort, numberscount);
     CheckIsSorted(arraytosort, numberscount);
-    
+    MergeSort(arraytosort, 0, numberscount-1);
+    CheckIsSorted(arraytosort, numberscount);
+    //PrintArray(arraytosort, numberscount);
     return 0;
 }
 
@@ -57,14 +62,72 @@ void CheckIsSorted(double A[], size_t n)
         }
         if(A[i+1] < A[i])
         {
-            printf("Array is not sorted\n");
+            printf("\nArray is not sorted\n");
             return;
         }
     }
-    printf("Array is sorted\n");
+    printf("\nArray is sorted\n");
 }
 
 
-void TopDownMergeSort(double A[], double B[], size_t n)
+void MergeSort(double A[], size_t l, size_t r)
 {
+    if(l < r)
+    {
+        size_t m = l + (r - l) / 2;
+        MergeSort(A, l, m);
+        MergeSort(A, m + 1, r);
+        Merge(A, l, m, r);
+        
+    }
+    
+}
+
+void Merge(double A[], size_t l, size_t m, size_t r)
+{
+    size_t n1 = m - l + 1;
+    size_t n2 = r - m;
+    
+    size_t i;
+    size_t j;
+    size_t k = l;
+    
+    double L[n1];
+    double R[n2];
+    
+    for (i = 0; i < n1; i++) {
+        L[i] = A[l + i];
+    }
+    
+    for (j = 0; j < n2; j++) {
+        R[j] = A[m + 1 + j];
+    }
+    
+    i = 0;
+    j = 0;
+    
+    while (i < n1 && j < n2) {
+        if(L[i] <= R[j])
+        {
+            A[k] = L[i];
+            i++;
+        } else
+        {
+            A[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    
+    while (i < n1) {
+        A[k] = L[i];
+        i++;
+        k++;
+    }
+    
+    while (j < n2) {
+        A[k] = R[j];
+        j++;
+        k++;
+    }
 }
